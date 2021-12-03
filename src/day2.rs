7f95cf7bb729775a4bc5b0fe913598e5ab33f8a4
William Gough <williamhenrygough@gmail.com>
2021-12-02 19:36:39 -0600

    refactor day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,65 +1,102 @@
-struct Ship {
-    current_depth: i32,
-    current_horizontal_position: i32,
-    current_aim: i32,
+pub struct Ship {
+    pub current_depth: i32,
+    pub current_horizontal_position: i32,
+    pub current_aim: i32,
+}
+
+pub enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+pub struct Instruction {
+    pub direction: Direction,
+    pub units: i32,
+}
+
+impl Ship {
+    pub fn find_position(&self) -> i32 {
+        self.current_horizontal_position * self.current_depth
+    }
+
+    pub fn process_part_2(self, instr: &Instruction) -> Self {
+        match instr.direction {
+            Direction::Forward => Self {
+                current_horizontal_position: self.current_horizontal_position + instr.units,
+                current_depth: self.current_depth + (self.current_aim * instr.units),
+                ..self
+            },
+            Direction::Down => Self {
+                current_aim: self.current_aim + instr.units,
+                ..self
+            },
+            Direction::Up => Self {
+                current_aim: self.current_aim - instr.units,
+                ..self
+            },
+        }
+    }
+
+    pub fn process_part_1(self, instr: &Instruction) -> Self {
+        match instr.direction {
+            Direction::Forward => Self {
+                current_horizontal_position: self.current_horizontal_position + instr.units,
+                ..self
+            },
+            Direction::Down => Self {
+                current_depth: self.current_depth - instr.units,
+                ..self
+            },
+            Direction::Up => Self {
+                current_depth: self.current_depth + instr.units,
+                ..self
+            },
+        }
+    }
+}
+
+impl Default for Ship {
+    fn default() -> Self {
+        Self {
+            current_depth: 0,
+            current_horizontal_position: 0,
+            current_aim: 0,
+        }
+    }
 }
 
 #[aoc_generator(day2)]
-pub fn parse(input: &str) -> Vec<(String, i32)> {
+pub fn parse(input: &str) -> Vec<Instruction> {
     input
         .lines()
         .map(|line| {
             let mut parts = line.split(' ');
-            let direction = parts.next().unwrap().to_owned();
+            let direction = match parts.next().unwrap() {
+                "forward" => Direction::Forward,
+                "down" => Direction::Down,
+                "up" => Direction::Up,
+                _ => Direction::Forward,
+            };
             let units = parts.next().unwrap().parse::<i32>().unwrap();
-            (direction, units)
+
+            Instruction { direction, units }
         })
         .collect()
 }
 
 #[aoc(day2, part1)]
-pub fn solve_part1(input: &[(String, i32)]) -> i32 {
-    let mut submarine = Ship {
-        current_depth: 0,
-        current_horizontal_position: 0,
-        current_aim: 0,
-    };
-
-    input.iter().for_each(|(d, u)| {
-        let dir = d.as_str();
-        match dir {
-            "forward" => {
-                submarine.current_horizontal_position += u;
-            }
-            "down" => submarine.current_depth -= u,
-            "up" => submarine.current_depth += u,
-            _ => {}
-        };
-    });
-
-    submarine.current_horizontal_position * submarine.current_depth
+pub fn solve_part1(instructions: &[Instruction]) -> i32 {
+    instructions
+        .iter()
+        .fold(Ship::default(), Ship::process_part_1)
+        .find_position()
 }
 
 #[aoc(day2, part2)]
-pub fn solve_part2(input: &[(String, i32)]) -> i32 {
-    let mut submarine = Ship {
-        current_depth: 0,
-        current_horizontal_position: 0,
-        current_aim: 0,
-    };
-
-    input.iter().for_each(|(d, u)| {
-        let dir = d.as_str();
-        match dir {
-            "forward" => {
-                submarine.current_horizontal_position += u;
-                submarine.current_depth += submarine.current_aim * u;
-            }
-            "down" => submarine.current_aim += u,
-            "up" => submarine.current_aim -= u,
-            _ => {}
-        };
-    });
-
-    submarine.current_horizontal_position * submarine.current_depth
+pub fn solve_part2(instructions: &[Instruction]) -> i32 {
+    instructions
+        .iter()
+        .fold(Ship::default(), Ship::process_part_2)
+        .find_position()
 }
